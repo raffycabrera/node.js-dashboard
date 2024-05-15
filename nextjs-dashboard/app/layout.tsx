85846170fca8bd8471e9fff21945945c542e2a89
Rafael Cabrera <raffeyrera@gmail.com>
2024-05-15 19:34:46 +0800

    First commit

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts.ts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+        
+      <body classname = {`${inter.className} antialiased`}>{children}</body>
+
+    </html>
+  );
+}
